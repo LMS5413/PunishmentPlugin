f864d1cba3115d5bf7337446b8e0b720f99dbac7
LMS5413 <lms5413@outlook.com>
2025-05-16 14:54:31 -0300

    remove deprecated event

--- src/main/kotlin/com/github/lms5413/punishment/events/PlayerEvent.kt
+++ src/main/kotlin/com/github/lms5413/punishment/events/PlayerEvent.kt
@@ -8,7 +8,6 @@ import com.github.lms5413.punishment.utils.kickPlayer
 import io.papermc.paper.event.player.AsyncChatEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.bukkit.event.player.PlayerJoinEvent
 
 class PlayerEvent: Listener {
